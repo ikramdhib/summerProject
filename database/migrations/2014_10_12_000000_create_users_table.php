d5799c20eb7720bf9ea1c42000428e6757a04247
ikramdhib <dhibikram50@gmail.com>
2022-08-21 21:10:00 +0100

    register user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('nom');
+            $table->string('tel');
+            $table->string('adresse')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('mdp');
             $table->rememberToken();
             $table->timestamps();
         });
